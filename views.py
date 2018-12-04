35ffa4c6f894840ffc566ee31d8a66c7846ad58a
Fizza Usama <fizzakamran94@gmail.com>
2018-12-04 21:45:27 +0000

    Added categories.html and viewed all categories

--- views.py
+++ views.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from models import Base, Category, CategoryItem
 app=Flask(__name__)
+
+
+# Connect to Database and create database session
+engine = create_engine('sqlite:///catalogapp.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 def index():
-    return 'Welcome!'
+    categories=session.query(Category).order_by(asc(Category.name))
+    return render_template('categories.html',categories=categories)
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,threaded=False)
