7eeafe96658e7131e30913984844eebd4e4b3487
Fizza Usama <fizzakamran94@gmail.com>
2018-12-20 10:02:26 +0000

    Login with google,add User and generate auth token

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,flash,redirect,url_for,jsonify
 from sqlalchemy import create_engine, asc,desc
 from sqlalchemy.orm import sessionmaker
-from models import Base, Category, CategoryItem
+from models import Base, Category, CategoryItem,User
 
 
 from flask_httpauth import HTTPBasicAuth
@@ -9,6 +9,12 @@ import json
 from flask import session as login_session
 import random
 import string
+
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+from flask import make_response
+import requests
 
 app=Flask(__name__)
 CLIENT_ID = json.loads(
@@ -37,9 +43,105 @@ def login(provider):
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    access_token=request.json.get('item')
-    return "access token received %s " % access_token
+    auth_code=request.json.get('item')
+    if provider =='google':
+        try:
+            # Upgrade the authorization code into a credentials object
+            oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+            oauth_flow.redirect_uri = 'postmessage'
+            credentials = oauth_flow.step2_exchange(auth_code)
+        except FlowExchangeError:
+            response = make_response(
+                json.dumps('Failed to upgrade the authorization code.'), 401)
+            response.headers['Content-Type'] = 'application/json'
+            return response
 
+        # Check that the access token is valid.
+        access_token = credentials.access_token
+        url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+            % access_token)
+        h = httplib2.Http()
+        result = json.loads(h.request(url, 'GET')[1])
+        # If there was an error in the access token info, abort.
+        if result.get('error') is not None:
+            response = make_response(json.dumps(result.get('error')), 500)
+            response.headers['Content-Type'] = 'application/json'
+            return response
+
+        # Verify that the access token is used for the intended user.
+        gplus_id = credentials.id_token['sub']
+        if result['user_id'] != gplus_id:
+            response = make_response(
+                json.dumps("Token's user ID doesn't match given user ID."), 401)
+            response.headers['Content-Type'] = 'application/json'
+            return response
+
+        # Verify that the access token is valid for this app.
+        if result['issued_to'] != CLIENT_ID:
+            response = make_response(
+                json.dumps("Token's client ID does not match app's."), 401)
+            print "Token's client ID does not match app's."
+            response.headers['Content-Type'] = 'application/json'
+            return response
+
+        stored_access_token = login_session.get('access_token')
+        stored_gplus_id = login_session.get('gplus_id')
+        if stored_access_token is not None and gplus_id == stored_gplus_id:
+            response = make_response(json.dumps('Current user is already connected.'),
+                                    200)
+            response.headers['Content-Type'] = 'application/json'
+            return response
+
+        # Store the access token in the session for later use.
+        login_session['access_token'] = credentials.access_token
+        login_session['gplus_id'] = gplus_id
+
+        # Get user info
+        userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+        params = {'access_token': credentials.access_token, 'alt': 'json'}
+        answer = requests.get(userinfo_url, params=params)
+
+        data = answer.json()
+
+        login_session['username'] = data['name']
+        login_session['picture'] = data['picture']
+        login_session['email'] = data['email']
+
+
+        login_session['provider'] = 'google'
+        # see if user exists, if it doesn't make a new one
+        user= getUser(data["email"])
+        if not user:
+            user = createUser(login_session)
+        login_session['user_id'] = user.id
+        
+        #make token
+        token = user.generate_auth_token(600)
+
+        
+        flash("You are now logged in as %s" % login_session['username'])
+        #send back token to the client 
+        return jsonify({'token': token.decode('ascii')})
+        
+        #return jsonify({'token': token.decode('ascii'), 'duration': 600})
+    else:
+        return 'Unrecoginized Provider'
+
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    #user = session.query(User).filter_by(email=login_session['email']).one()
+    return newUser
+
+def getUser(email):
+        user = session.query(User).filter_by(email=email).one()
+        if user is not None:
+            return user
+        else:
+            return None
 @app.route('/catalog/<string:category_name>/<int:item_id>/JSON')
 def itemJSON(category_name,item_id):
     item=session.query(CategoryItem).filter_by(id=item_id).one()
