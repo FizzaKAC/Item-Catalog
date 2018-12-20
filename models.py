ae3e5c12bf246fa9cc5a9d1bc4c79f595a34cdff
Fizza Usama <fizzakamran94@gmail.com>
2018-12-20 10:01:06 +0000

    Generate and verify auth token for user

--- models.py
+++ models.py
@@ -3,8 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 import datetime
+import random, string
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+
 
 class User(Base):
     __tablename__ = 'user'
@@ -14,6 +18,23 @@ class User(Base):
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in = expiration)
+        return s.dumps({'id': self.id })
+
+    @staticmethod
+    def verify_auth_token(token):
+    	s = Serializer(secret_key)
+    	try:
+    		data = s.loads(token)
+    	except SignatureExpired:
+    		#Valid Token, but expired
+    		return None
+    	except BadSignature:
+    		#Invalid Token
+    		return None
+    	user_id = data['id']
+    	return user_id
 class Category(Base):
     __tablename__='category'
 
